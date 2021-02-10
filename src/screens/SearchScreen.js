8a0aeb27d1f2abbd6f426bc72548d91e0cc5eb54
mike <mikesapienzaaudio@gmail.com>
2021-02-10 14:24:57 -0600

    created reusable api hook

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,34 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import SearchBar from "../components/SearchBar";
-import yelp from "../api/yelp";
+import useResults from "../hooks/useResults";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
-  const [results, setResults] = useState([]);
-  const [error, setError] = useState("");
-
-  const searchApi = async () => {
-    try {
-      const response = await yelp.get("/search", {
-        params: {
-          limit: 50,
-          term,
-          location: "Chicago, IL",
-        },
-      });
-      setResults(response.data.businesses);
-    } catch (err) {
-      setError("Something went wrong...");
-    }
-  };
+  const [searchApi, results, error] = useResults();
 
   return (
     <View>
       <SearchBar
         term={term}
         onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={() => searchApi()}
+        onTermSubmit={() => searchApi(term)}
       />
       {error ? <Text>{error}</Text> : null}
       <Text>We have found {results.length} results</Text>
