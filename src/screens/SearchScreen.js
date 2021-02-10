e7aa94b157510dbe610a490c5b174107a9254888
mike <mikesapienzaaudio@gmail.com>
2021-02-10 15:37:47 -0600

    tutorial finished, need to add features to results show screen like phone and address

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,22 +1,33 @@
-import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 import SearchBar from "../components/SearchBar";
+import ResultsList from "../components/ResultsList";
 import useResults from "../hooks/useResults";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [searchApi, results, error] = useResults();
 
+  const filterResultsByPrice = (price) => {
+    return results.filter((result) => {
+      return result.price === price;
+    });
+  };
+
   return (
-    <View>
+    <>
       <SearchBar
         term={term}
         onTermChange={(newTerm) => setTerm(newTerm)}
         onTermSubmit={() => searchApi(term)}
       />
       {error ? <Text>{error}</Text> : null}
-      <Text>We have found {results.length} results</Text>
-    </View>
+      <ScrollView>
+        <ResultsList header="$" results={filterResultsByPrice("$")} />
+        <ResultsList header="$$" results={filterResultsByPrice("$$")} />
+        <ResultsList header="$$$" results={filterResultsByPrice("$$$")} />
+      </ScrollView>
+    </>
   );
 };
 
